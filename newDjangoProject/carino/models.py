a113a6860b57726e4396be6c98e42aa052426b9a
Mehdi Jahani <80170150+mehdijahani1998@users.noreply.github.com>
2022-08-06 15:41:41 +0430

    updated models.py from local source.

--- newDjangoProject/carino/models.py
+++ newDjangoProject/carino/models.py
@@ -1,3 +1,38 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+
+from django.db import models
+from django.utils import timezone
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    
+    def __str__(self) -> str:
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.choice_text
+
+
+class Car(models.Model):
+    manufacturer = models.CharField(max_length=50)
+    car_model = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    price = models.IntegerField()
+    production_year = models.IntegerField()
+    mileage = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.manufacturer + '-' + self.car_model + '-' + str(self.price) + '-' + str(self.production_year)
+
+
